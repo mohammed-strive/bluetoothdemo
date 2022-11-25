64ccf154b877b18203ea3cca154f0fde2dbed316
Harsh Vardhan <harsh.vardhan1@thoughtworks.com>
2022-11-25 17:32:44 +0530

    bugfix: add write characteristics to the scanning filter of iOS
    * iOS wants it's programmers to only have the characteristics they actually need
       to optimise for battery performance
    
    Co-Authored-by: Mohammed Jaffer <mohammed.strive@outlook.com>
    
    Signed-off-by: Harsh Vardhan <harsh.vardhan1@thoughtworks.com>

--- bluetoothdemo/TransferServiceController.swift
+++ bluetoothdemo/TransferServiceController.swift
@@ -11,5 +11,7 @@ import CoreBluetooth
 struct TransferService {
     static let serviceUUID = CBUUID(string: "0000AB29-0000-1000-8000-00805f9b34fb")
     static let characteristicUUID = CBUUID(string: "00002032-0000-1000-8000-00805f9b34fb")
+    static let writeCharacteristic = CBUUID(string: "00002031-0000-1000-8000-00805f9b34fb")
+
 }
 
